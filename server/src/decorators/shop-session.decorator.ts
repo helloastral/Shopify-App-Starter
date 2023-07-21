a853da5f6dcdaa7f24a853ba0f0f0f49299e27bd
Geon George <geongeorge98@gmail.com>
2023-07-21 12:56:39 +0530

    Adds new decorator for session

---
+++ server/src/decorators/shop-session.decorator.ts
@@ -0,0 +1,17 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+/**
+ * This decorator is used to inject the shopify session into a controller method.
+ * There is no arguments required. Simply use `@ShopSession() session` in your controller method.
+ */
+
+export const ShopSession = createParamDecorator(
+  async (_: unknown, ctx: ExecutionContext) => {
+    const http = ctx.switchToHttp();
+    const response = http.getResponse();
+
+    const session = response.locals.shopify.session;
+
+    return session;
+  },
+);
